c36923c354bb23210d06e7cfe4d6afab1d8808ce
Thomas <thomas.lachieze@gmail.com>
2020-07-14 17:58:45 +0200

    First form for signup component

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,36 +1,102 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Modal, Form, Col, Row } from 'react-bootstrap';
+import axios from 'axios';
 import '../App.css';
 import './Signup.css';
 
 const Signup = () => {
+  const [inputs, setInputs] = useState({
+    firstname: '',
+    lastname: '',
+    pseudo: '',
+    email: '',
+    password: '',
+    avatar: '',
+    newsletter_subscriber: 0,
+  });
+
+  const [show, handleShow] = useState(false);
+
+  const submitForm = (event) => {
+    event.preventDefault();
+    const url = 'http://localhost:3000/api/games';
+    axios
+      .post(url, inputs)
+      .then((res) => res.data)
+      .catch((e) => {
+        // eslint-disable-next-line no-alert
+        alert(`Erreur lors de l'ajout du jeu : ${e.message}`);
+      });
+  };
+
+  const onChange = (e) => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  console.log(inputs);
+
   return (
-    <section id="signup">
-      <div class="col-12">
-        <h2>Je m'inscris direct !</h2>
-        <div class="container-md">
-          <form>
-            <div class="form-group">
-              <input type="text" class="form-control" id="name" placeholder="Pseudo de Geekoss"/>
-            </div>
-            <div class="form-group">
-              <input type="email" class="form-control" id="email" placeholder="Email"/>
+    <>
+      <Modal size="lg" show={show} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Ton compte a bien été créé !</Modal.Title>
+        </Modal.Header>
+        <Modal.Footer>
+          <button type="button" className="ButtonAction Action">
+            Ok
+          </button>
+        </Modal.Footer>
+      </Modal>
+      <section id="signup">
+        <div class="col-12">
+          <h2>Je m'inscris direct !</h2>
+          <div class="container-md">
+
+            <Form onSubmit={submitForm}>
+              <Form.Group onChange={onChange}>
+                <Form.Control type="text" name="pseudo" placeholder="Pseudo de Geekoss"/>
+              </Form.Group>
+              <Row>
+                <Col>
+                  <Form.Group onChange={onChange}>
+                    <Form.Control name="firstname"  placeholder="Prénom"/>
+                  </Form.Group>
+                </Col>
+                <Col>
+                  <Form.Group onChange={onChange}>
+                    <Form.Control name="lastname"  placeholder="Nom"/>
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Form.Group onChange={onChange}>
+                <Form.Control type="text" name="email" placeholder="Email"/>
+              </Form.Group>
+              <Form.Group onChange={onChange}>
+                <Form.Control type="password" name="password" placeholder="Mot de passe"/>
+              </Form.Group>
+              <div class="form-check">
+                <input type="checkbox" class="form-check-input" checked={inputs.newsletter_subscriber} name="newsletter_subscriber" onChange={onChange}/>
+                <label class="form-check-label" for="newsletter_subscriber">Je m'inscris à la newsletter</label>
               </div>
-            <div class="form-group">
-                <input type="password" class="form-control" id="email" placeholder="Mot de passe"/>
-            </div>
-            <div class="form-check">
-              <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-              <label class="form-check-label" for="exampleCheck1">Je m'inscris à la newsletter</label>
-            </div>
-            <p>En m'inscrivant je confirme avoir lu et accepté les <a href="terms.html">Termes & Conditions et Politique de confidentialité</a> de Geekoss. Je confirme avoir au moins 18 ans.</p>
-            <div class="form-group">
-              <button type="submit" id="send" class="btn btn-primary">Inscription</button>
-            </div>
-          </form>
+              <p>En m'inscrivant je confirme avoir lu et accepté les <a href="terms.html">Termes & Conditions et Politique de confidentialité</a> de Geekoss. Je confirme avoir au moins 18 ans.</p>
+              <div className="d-flex justify-content-center">
+                <button
+                  className="btn btn-primary"
+                  type="submit"
+                  onClick={() => handleShow(true)}
+                >
+                  Inscription
+                </button>
+              </div>
+            </Form>
+          </div>
         </div>
-      </div>
-      <p class="d-flex justify-content-center"><a href="#">C'est compliqué, aidez-moi !</a></p>
-    </section>
+        <p class="d-flex justify-content-center"><a href="#">C'est compliqué, aidez-moi !</a></p>
+      </section>
+    </>
   )
 };
 
