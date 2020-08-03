936df2948192c67eeac0a92ba198a039ecc55305
Thomas <thomas.lachieze@gmail.com>
2020-08-03 11:59:34 +0200

    Adding Legal Notice

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import './App.css';
-import { Route, Switch } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import AuthProvider from './components/contexts/AuthContext';
-import PrivateRoute from './components/PrivateRoute';
-import Header from './components/Header';
-import Homepage from './components/Homepage';
-import Signup from './components/Signup';
-import Login from './components/Login';
-import Footer from './components/Footer';
-import AddOffer from './components/AddOffer';
-import UserAccount from './components/UserAccount';
-import OfferDetails from './components/OfferDetails';
+import React from "react";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AuthProvider from "./components/contexts/AuthContext";
+import PrivateRoute from "./components/PrivateRoute";
+import Header from "./components/Header";
+import Homepage from "./components/Homepage";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Footer from "./components/Footer";
+import AddOffer from "./components/AddOffer";
+import UserAccount from "./components/UserAccount";
+import OfferDetails from "./components/OfferDetails";
+import LegalNotice from "./components/LegalNotice";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
           <Route exact path="/login" component={Login} />
           <Route exact path="/add-offer" component={AddOffer} />
           <Route exact path="/offer-details/:id" component={OfferDetails} />
+          <Route exact path="/legal-notice" component={LegalNotice} />
           <PrivateRoute exact path="/user-account" component={UserAccount} />
         </Switch>
         <Footer />
--- src/components/Action.jsx
+++ src/components/Action.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../App.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
 
 const Action = () => {
   return (
     <section id="actions">
-            <div className="col-12">
-                <h2>On se revend nos trésors entre Geekoss !</h2>
-                <div className="d-flex justify-content-center">
-                  <Link className="hidden-sm" to="/add-offer">
-                    <button type="button" className="btn btn-primary">Je vends du lourd</button>
-                  </Link>
-                </div>
-                <p className="d-flex justify-content-center"><a href="#">Comment ça marche ?</a></p>
-            </div>
-        </section>
-  )
+      <div className="col-12">
+        <h2>On se revend nos trésors entre Geekoss !</h2>
+        <div className="d-flex justify-content-center">
+          <Link className="hidden-sm" to="/add-offer">
+            <button type="button" className="btn btn-primary">
+              Je vends du lourd
+            </button>
+          </Link>
+        </div>
+        <p className="d-flex justify-content-center">
+          <a href="www.google.fr">Comment ça marche ?</a>
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Action;
