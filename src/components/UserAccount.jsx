0743b77b8be2d6346f3ba0d38d664847907ef58d
Thomas <thomas.lachieze@gmail.com>
2020-08-05 10:25:58 +0200

    Updating UserAccount with Redux

--- src/components/UserAccount.jsx
+++ src/components/UserAccount.jsx
@@ -8,14 +8,12 @@ import OffersUser from "./OffersUser/OffersUser";
 import "../App.css";
 import "./Signup.css";
 
-const UserAccount = ({ dispatch }) => {
+const UserAccount = ({ dispatch, informations }) => {
   const { auth } = useContext(authContext);
-  const [inputs, setInputs] = useState([]);
   const [inputsToSend, setInputsToSend] = useState({
     avatar: "",
   });
   const [show, handleShow] = useState(false);
-  const [connect, setConnect] = useState(true);
 
   const defaultImg =
     "https://api.drupal.org/sites/default/files/default-avatar.png";
@@ -30,16 +28,13 @@ const UserAccount = ({ dispatch }) => {
     }).then((res) => {
       dispatch(setUserInfo(res.data.authData.admin[0]));
     });
-    // .then((response) => response.data)
-    // .then((response) => console.log(response.data))
-    // .then((data) => setProfile(data.authData.admin[0]));
   });
 
-  // const submitForm = (event) => {
-  //   event.preventDefault();
-  //   const url = `http://localhost:3000/api/users/${Profile.id}`;
-  //   axios.put(url, inputsToSend).then((res) => res.data);
-  // };
+  const submitForm = (event) => {
+    event.preventDefault();
+    const url = `http://localhost:3000/api/users/${informations.id}`;
+    axios.put(url, inputsToSend).then((res) => res.data);
+  };
 
   const onChange = (e) => {
     setInputsToSend({
@@ -48,37 +43,8 @@ const UserAccount = ({ dispatch }) => {
     });
   };
 
-  const handleConnect = () => {
-    // window.localStorage.setItem('id', JSON.stringify(Profile.id));
-    // window.localStorage.setItem('firstname', JSON.stringify(Profile.firstname));
-    // window.localStorage.setItem('lastname', JSON.stringify(Profile.lastname));
-    // window.localStorage.setItem('pseudo', JSON.stringify(Profile.pseudo));
-    // window.localStorage.setItem('avatar', JSON.stringify(Profile.avatar));
-    axios
-      .get(
-        `http://localhost:3000/api/users/${window.localStorage.getItem("id")}`
-      )
-      .then((response) => response.data)
-      .then((data) => setInputs(data));
-    setConnect(false);
-  };
-  console.log(localStorage.getItem("avatar"));
   return (
     <>
-      <Modal size="lg" show={connect} centered>
-        <Modal.Header closeButton>
-          <Modal.Title>Content de te revoir mon ami !</Modal.Title>
-        </Modal.Header>
-        <Modal.Footer>
-          <button
-            type="button"
-            onClick={() => handleConnect()}
-            className="ButtonAction Action"
-          >
-            Ok
-          </button>
-        </Modal.Footer>
-      </Modal>
       <Modal size="lg" show={show} centered>
         <Modal.Header closeButton>
           <Modal.Title>Ta photo a bien été modifiée !</Modal.Title>
@@ -99,16 +65,17 @@ const UserAccount = ({ dispatch }) => {
           <div className="row">
             <div className="col-4 UserBoard-Left">
               <div className="Avatar">
-                {inputs[0] && !inputs[0]["avatar"] === null ? (
-                  <img src={localStorage.getItem("avatar")} alt="avatar" />
+                {informations.avatar ? (
+                  <img src={informations.avatar} alt="avatar" />
                 ) : (
                   <img src={defaultImg} alt="avatar" />
                 )}
               </div>
-              <h2>{/* {Profile.firstname} {Profile.lastname} */}</h2>
-              {/* ADD onSubmit={submitForm} */}
+              <h2>
+                {informations.firstname} {informations.lastname}
+              </h2>
               <Form>
-                <Form.Group onChange={onChange}>
+                <Form.Group onChange={onChange} onSubmit={submitForm}>
                   <Form.Label>Modifie ton avatar</Form.Label>
                   <Form.Control
                     type="text"
@@ -136,4 +103,8 @@ const UserAccount = ({ dispatch }) => {
   );
 };
 
-export default connect()(UserAccount);
+const mapStateToProps = (state) => ({
+  informations: state.SetInfoUserRedux.inputs,
+});
+
+export default connect(mapStateToProps)(UserAccount);
